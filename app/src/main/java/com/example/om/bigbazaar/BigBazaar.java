3607f01ec78ac9358d9805a8a52f7b8d2cc5826c
ashima-verma <ashimaverma2009@gmail.com>
2017-06-06 10:39:59 +0530

    Single screen: initial commit

---
+++ app/src/main/java/com/example/om/bigbazaar/BigBazaar.java
@@ -0,0 +1,13 @@
+package com.example.om.bigbazaar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BigBazaar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_big_bazaar);
+    }
+}
